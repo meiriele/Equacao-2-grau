0cba4c8014336984923783002a47861c2aef2812
ice <ice@lab06m34.nrc.ice.ufjf.br>
2017-08-17 21:31:52 -0300

    botao default

---
+++ src/ufjf/br/dcc171/Aula04Exm01.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ufjf.br.dcc171;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ice
+ */
+public class Aula04Exm01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JanelaBotoes janela = new JanelaBotoes();
+        janela.setSize(200, 300);
+        janela.setLocationRelativeTo(null);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setVisible(true);
+    }
+    
+}
---
+++ src/ufjf/br/dcc171/JanelaBotoes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ufjf.br.dcc171;
+
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ice
+ */
+public class JanelaBotoes extends JFrame {
+    public JanelaBotoes() throws HeadlessException {
+        super("Botoes");
+    }
+}
